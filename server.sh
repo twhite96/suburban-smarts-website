8eb982765cb5b875a8ade5a7fc91fce16e788459
Tiffany <hey@tiffanyrwhite.com>
2021-09-19 21:19:36 -0400

    why

--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-bundle exec jekyll serve --livereload --open-url --drafts --future --port 4004 --host 0.0.0.0
+bundle exec jekyll serve --livereload --open-url --drafts --future --port 4004 --host 0.0.0.0 JEKYLL_ENV=production
